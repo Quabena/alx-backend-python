7a96678b82c742f97a9eb50c24f9a3e40222e411
Evans <ivanz.corbie@gmail.com>
2025-04-24 20:03:11 +0000

    4-define_variables.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module defines and annotates basic variables using
+Python type annotations.
+It demonstrates assignment of int, float, bool, and
+str types.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "ALX"
