d197801d737e5aae348508c3dfedd473c38a5110
Evans <ivanz.corbie@gmail.com>
2025-04-30 00:58:59 +0000

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ A module that defines an asynchronous generator.
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Coroutine that yields 10 random numbers between 0 and 10
 
